583fac7e8b117162fbbb375fbbb945a32f62de48
Georges Limouzy <georges.limouzy@smile.fr>
2020-09-02 16:37:06 +0200

    Created user creation tests

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -6,22 +6,34 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
-class UserType extends AbstractType
-{
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-        $builder
-            ->add('username')
-            ->add('password')
-            ->add('admin')
-        ;
-    }
+class UserType extends AbstractType {
 
-    public function configureOptions(OptionsResolver $resolver)
-    {
-        $resolver->setDefaults([
-            'data_class' => User::class,
-        ]);
+  /**
+   * @var AuthorizationCheckerInterface
+   */
+  protected $authorizationChecker;
+
+  public function __construct(AuthorizationCheckerInterface $authorizationChecker) {
+    $this->authorizationChecker = $authorizationChecker;
+  }
+
+  public function buildForm(FormBuilderInterface $builder, array $options) {
+    $builder
+      ->add('username')
+      ->add('password');
+
+    if ($this->authorizationChecker->isGranted('ROLE_ADMIN')) {
+      $builder->add('admin');
     }
+  }
+
+  public function configureOptions(OptionsResolver $resolver) {
+    $resolver->setDefaults(
+      [
+        'data_class' => User::class,
+      ]
+    );
+  }
 }
