3670dc2ad0c195576e4bf782718cc575fcf18e0f
Adrien LUCAS <adrien.lucas@smile.fr>
2020-09-03 15:31:22 +0200

    Add voter tests

--- tests/UserCreationTest.php
+++ tests/UserCreationTest.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace App\Tests;
 
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -61,5 +63,67 @@ class UserCreationTest extends WebTestCase
         ]);
 
         $this->assertSelectorTextContains('form', 'This form should not contain extra fields');
+    }
+
+    public function testAWebsiteUserCanEditADatabaseUserWhenItsNotAnAdmin()
+    {
+        $client = self::createClient([], [
+            'PHP_AUTH_USER' => 'john',
+            'PHP_AUTH_PW'   => 'j0hn',
+        ]);
+
+        $this->loadUserFixture('toto', 'john', false);
+
+        $client->request('GET', '/user/toto');
+
+        $this->assertSelectorExists('input[name="user[username]"]');
+        $this->assertSelectorExists('input[name="user[password]"]');
+    }
+
+    public function testAWebsiteUserCanNotEditADatabaseUserWhenItsAnAdmin()
+    {
+        $client = self::createClient([], [
+            'PHP_AUTH_USER' => 'john',
+            'PHP_AUTH_PW'   => 'j0hn',
+        ]);
+
+
+        $this->loadUserFixture('toto', 'john', true);
+
+        $client->request('GET', '/user/toto');
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_FORBIDDEN);
+    }
+
+    public function testAWebsiteUserCanNotEditADatabaseUserWhenItWasNotCreatedByHim()
+    {
+        $client = self::createClient([], [
+            'PHP_AUTH_USER' => 'john',
+            'PHP_AUTH_PW'   => 'j0hn',
+        ]);
+
+        $this->loadUserFixture('toto', 'fabien', false);
+        $client->request('GET', '/user/toto');
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_FORBIDDEN);
+    }
+
+    private function loadUserFixture(string $username, string $owner, bool $isAdmin = false)
+    {
+        $entityManager = self::$container->get(EntityManagerInterface::class);
+
+        // Empty the database tables user AND messenger_messages before the test
+        $entityManager
+            ->getConnection()
+            ->exec('DELETE FROM user');
+
+        $user = new User();
+        $user->setUsername($username);
+        $user->setPassword($username);
+        $user->setAdmin($isAdmin);
+        $user->setOwner($owner);
+
+        $entityManager->persist($user);
+        $entityManager->flush();
     }
 }
